9348421cb5dfef6f7ac8c67795892fec3ea99419
Matthew G (Desktop) <49723415+Matthew-E-Gould@users.noreply.github.com>
2022-04-01 10:47:36 +0100

    tweaks

--- main.py
+++ main.py
@@ -7,64 +7,92 @@ def writeToJSONFile(path, fileName, data):
     with open(filePathNameWExt, 'w') as fp:
         json.dump(data, fp, ensure_ascii=False)
 
+# const
+donePhrase = "-noMoreKeys"
+
 # init
-initialLinks = ['https://telephone-exchange.co.uk/sitemap.html','https://telephone-exchange.co.uk/sitemap2.html','https://telephone-exchange.co.uk/sitemap3.html','https://telephone-exchange.co.uk/sitemap4.html','https://telephone-exchange.co.uk/sitemap5.html','https://telephone-exchange.co.uk/sitemap6.html','https://telephone-exchange.co.uk/sitemap7.html','https://telephone-exchange.co.uk/sitemap8.html','https://telephone-exchange.co.uk/sitemap9.html','https://telephone-exchange.co.uk/sitemap10.html','https://telephone-exchange.co.uk/sitemap11.html']
-preUrl = 'https://telephone-exchange.co.uk'
-ADDRESS_ELEMENT_CONDITION = 'Located at'
-problemLocationLinks = []
-data = []
+baseSite = ""
+keywords = []
+urlsToAccess = []
+accessedUrls = []
+externalUrls = []
+keywordUrls = []
+allowPreWildcards = False
+allowPostWildcards = False
+allowExplorationOfExternalLinks = False
+externalSearchDepth = 0
+keywordCount = 0
 
-# time to scapre
-for initialLink in initialLinks:
-    print('reading '+initialLink) # output
-    r = requests.get(initialLink)
-    soup = BeautifulSoup(r.content, features="html.parser")
-    lsbb = soup.find('div', class_="left_section_box_body")
-    links = lsbb.find_all('a')
-    for link in links:
-        fullLocationLink = preUrl + link['href']
-        print('-reading '+fullLocationLink)
-        r = requests.get(fullLocationLink)
-        soup = BeautifulSoup(r.content, features="html.parser")
-        lsbbLocation = soup.find('div', class_="left_section_box_body")
-        p_tags = soup.find_all('p')
-        p_tags_iterator = -1
-        for p_tag in p_tags:
-            p_tags_iterator += 1
-            content = p_tag.encode_contents()
-            if(ADDRESS_ELEMENT_CONDITION in content):
-                break
+# get site to start from
+print("Enter base url you'd like to trawl:")
+baseSite = input()
+urlsToAccess.append(baseSite)
+print("Enter keyword " + str(keywordCount+1) + " that you'd like to search for :")
+keywords.append(input())
+keywordCount += 1
+inp = ""
 
-        if (p_tags_iterator > 0):
-            name = soup.find('div', class_="left_section_box_top").encode_contents() # exchange name
-            addr = p_tags[p_tags_iterator].encode_contents() # exchange address
+# getting keywords to look for
+while(inp != donePhrase):
+    print("Enter keyword " + str(keywordCount+1) + " that you'd like to search for (type '"+ donePhrase +"' when you've entered them all):")
+    inp = input()
+    keywordCount += 1
 
-            addrArrayPre = addr.split('<br/>')
-            addrArrayPost = []
-            first = True
-            for addrPre in addrArrayPre:
-                if first:
-                    first = False
-                else:
-                    addrArrayPost.append(addrPre.replace("\n", "").replace(",", "").replace(".", ""))
+# wildcard choice stuff here (TBI)
 
-            embedLinks = lsbbLocation.find_all('iframe')
-            googleMapsEmbed = embedLinks[0]['src']
-            bingMapsEmbed = embedLinks[1]['src']
-            data.append({
-                'url': fullLocationLink,
-                'name': name,
-                'address': addrArrayPost,
-                'googleMaps': googleMapsEmbed,
-                'bingMaps': bingMapsEmbed,
-            })
-        else:
-            print('--issue with the page '+fullLocationLink)
-            problemLocationLinks.append(fullLocationLink)
+for url in urlsToAccess:
+    print('reading '+url)
+    req = requests.get(url)
 
-print("writing data to file...")
-writeToJSONFile('./', 'data', data)
-writeToJSONFile('./', 'fails', problemLocationLinks)
-print("=========================================")
-print("Program complete, Press enter to close :)")
-raw_input()
+    # check for keyword
+    for keyword in keywords:
+        if keyword in str(req.content):
+            keywordUrls.append(url)
+            print('Found Keyword!')
+
+    # check for further links
+    soup = BeautifulSoup(req.content, features="html.parser")
+    tags = soup.find_all('a', href=True)
+    for tag in tags:
+        href = tag['href']
+
+        # filter out things (pre full URL)
+        if len(href) > 0:
+            notTag = href[0] != '#'
+            notTel = False
+            if len(href) >= 4 and href[:4] != 'tel:':
+                notTel = True
+            elif len(href) < 4:
+                notTel = True
+            notMailTo = False
+            if len(href) >= 7 and href[:7] != 'mailto:':
+                notMailTo = True
+            elif len(href) < 7:
+                notMailTo = True
+            # check href is URL and not URI
+            hrefURL = href
+            if href[0] == '/':
+                hrefURL = url + href
+
+            uniqueLink = hrefURL not in urlsToAccess and hrefURL not in accessedUrls
+            # implement filters
+            if notTag and notTel and notMailTo and uniqueLink:
+                print('found link: '+hrefURL)
+                urlsToAccess.append(hrefURL)
+
+    urlsToAccess.remove(url)
+    accessedUrls.append(url)
+
+    data = {
+        'baseSite': baseSite,
+        'keywords': keywords,
+        'urlsToAccess': urlsToAccess,
+        'accessedUrls': accessedUrls,
+        'externalUrls': externalUrls,
+        'keywordUrls': keywordUrls,
+    }
+
+    # save array to file
+    writeToJSONFile('./', 'data', data)
+
+print('done!')
